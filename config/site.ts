afd77dac0f40c3ab1555c30854dbdd998a2bf841
Limzy71 <muhikhsanjr05@gmail.com>
2026-07-07 23:04:03 +0700

    update: memperbarui link linkedin

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   location: "Indonesia",
   social: {
     github: "https://github.com/Limzy71",
-    linkedin: "https://www.linkedin.com/in/la-ode-muh-ikhsan-mbala-508628298/",
+    linkedin: "https://www.linkedin.com/in/laodemuhikhsanmbala/",
     instagram: "https://www.instagram.com/ikhsanlaode_/",
     email: "mailto:laodemuhikhsan18@gmail.com",
   },
